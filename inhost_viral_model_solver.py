cfc2315ee94280252d21608f4975612788135742
Yuanshen Liao <yuanshen.liao@yitu-inc.com>
2021-08-03 14:36:50 +0800

    restructure solver

--- inhost_viral_model_solver.py
+++ inhost_viral_model_solver.py
@@ -5,11 +5,12 @@
 # standard modules
 import argparse
 import json
+import logging
 import math
 import pathlib
 import pickle
 import sys
-from typing import Tuple
+from typing import Dict, List, Tuple
 
 # third-party modules
 import numpy as np
@@ -18,113 +19,6 @@ import numpy as np
 import diffusion3d_virus_town_large as dv
 
 
-def load_init_terms(gConf, hosts):
-    s = np.zeros((gConf['nx'], gConf['ny'], gConf['nz'], gConf['ng']))
-    T = np.zeros((gConf['nx'], gConf['ny'], gConf['nz'], gConf['ng']))
-
-    for h in hosts:
-        s[h['x'], h['y'], h['z'], [1, 5]] = h['lam']
-        T[h['x'], h['y'], h['z'], :] = (
-            h['T1_0_U'], h['T2_0_U'], h['I_0_U'], h['V_0_U'],
-            h['T1_0_L'], h['T2_0_L'], h['I_0_L'], h['V_0_L'],
-            gConf['env_0']
-        )
-
-    return s, T
-
-
-def update_A(A_diag, A_off_diag, hosts, T, t, gConf):
-    def delta(delta_I, sigma, mu, t):
-        return delta_I * np.e ** (sigma * (t - mu)) if (t >= mu) else delta_I
-
-    for h in hosts:
-        A_diag[h['x'], h['y'], h['z'], :] = (
-            # T1_U, T2_U
-            h['beta_U'] * T[h['x'], h['y'], h['z'], 3],
-            h['beta_U'] * T[h['x'], h['y'], h['z'], 3],
-            # I_U
-            delta(h['delta_I'], h['sigma_U'], h['mu'], t)
-            + h['w_U'] * T[h['x'], h['y'], h['z'], 1],
-            # V_U
-            h['c_U'],
-            # T1_L, T2_L
-            h['beta_L'] * T[h['x'], h['y'], h['z'], 7],
-            h['beta_L'] * T[h['x'], h['y'], h['z'], 7],
-            # I_L
-            delta(h['delta_I'], h['sigma_L'], h['mu'], t)
-            + h['w_L'] * T[h['x'], h['y'], h['z'], 5],
-            # V_L
-            h['c_L'],
-            # V_env
-            gConf['halflife'] + gConf['ventilation']
-        )
-
-        A_off_diag[h['x'], h['y'], h['z'],
-                   [2, 2, 3, 3, 6, 6, 7, 7, 8, 8],
-                   [0, 1, 2, 8, 4, 5, 3, 6, 3, 7]
-                   ] = (
-            -h['beta_U'] * T[h['x'], h['y'], h['z'], 3],
-            -h['beta_U'] * T[h['x'], h['y'], h['z'], 3],
-            -h['p_U'], -h['a_inhale'],
-            -h['beta_L'] * T[h['x'], h['y'], h['z'], 7],
-            -h['beta_L'] * T[h['x'], h['y'], h['z'], 7],
-            -h['a_conduct'], -h['p_L'],
-            -gConf['gamma_U'], -gConf['gamma_L']
-        )
-
-
-def inhost_viral_model_solver(conf, gConf, hosts):
-    """ An inhost viral model solver using `sim_time_steping_diffusion_calc`
-    to solve one or many within-host viral dynamic models simultaneously
-
-    input
-    =====
-    gConf: dict, values about the grid
-        nx, ny, nz, ng, ng2
-        error_solv, error_solv_ng, i_upwind, i_harmonic,
-        ndim_vel, nits, nits_solv_ng, relax
-        dx, dy, dz, dt, ntime
-
-    hostCells: dict, info about each host
-        position - x, y, z
-        parameters - T1_0, T2_0, I_0, V_0, beta, lam, delta, w, p, c
-    """
-    # initialise matrices
-    #
-    v_n = np.ones(gConf['ng'])
-    # advection-diffusion in the system
-    kdiff = np.zeros((gConf['nx'], gConf['ny'], gConf['nz'], gConf['ng']))
-    kdiff[:, :, :, -1] = 1.e8
-    # advection velocity
-    u = np.zeros((conf['ndim_vel'], gConf['nx']+1, gConf['ny']+1,
-                  gConf['nz']+1, gConf['ng']))
-    #
-    A_diag = np.zeros((gConf['nx'], gConf['ny'], gConf['nz'], gConf['ng']))
-    A_off_diag = np.zeros((gConf['nx'], gConf['ny'], gConf['nz'],
-                           gConf['ng'], gConf['ng2']))
-    # source term and initial values
-    s, T = load_init_terms(gConf, hosts)
-
-    time_list = [0]
-    T_list = [T]
-    for _ in range(conf['nstep']):
-        t = time_list[-1] + conf['dt'] * conf['ntime']
-        update_A(A_diag, A_off_diag, hosts, T, t, gConf)
-
-        T = dv.sim_time_steping_diffusion_calc(
-            T, conf['ntime'], conf['nits'], conf['nits_solv_ng'],
-            conf['relax'], gConf['error_solv'], gConf['error_solv_ng'],
-            gConf['dx'], gConf['dy'], gConf['dz'], conf['dt'],
-            v_n, A_diag, A_off_diag, kdiff, s, u,
-            gConf['i_upwind'], gConf['i_harmonic'],
-            conf['ndim_vel'], gConf['nx'], gConf['ny'], gConf['nz'],
-            gConf['ng'], gConf['ng2']
-        )
-
-        time_list.append(t)
-        T_list.append(T)
-
-    return np.array(T_list), time_list
 
 
 def parseInput() -> Tuple[argparse.Namespace, argparse.ArgumentParser]:
@@ -139,85 +33,291 @@ def parseInput() -> Tuple[argparse.Namespace, argparse.ArgumentParser]:
         help='path/to/model-conf/json/file'
     )
     parser.add_argument(
+        'hostsConf',
+        type=pathlib.Path,
+        help='path/to/host-conf/json/file'
+    )
+    parser.add_argument(
         'gridConf',
         type=pathlib.Path,
         help='path/to/grid-conf/json/file'
-    )
-    parser.add_argument(
-        'hostConf',
-        type=pathlib.Path,
-        help='path/to/host-conf/json/file'
     )
     args = parser.parse_args()
 
     return args, parser
 
 
-const_sec_per_day = 86400
+class Model:
+    constPath = 'conf/constant.json'
+
+    def __init__(
+        self,
+        modelConf: pathlib.Path,
+        hostsConf: pathlib.Path,
+        gridConf: pathlib.Path
+    ):
+        Model.loadConst()
+        Model.loadModel(modelConf)
+        Model.loadHosts(hostsConf)
+        Model.loadGrid(gridConf)
+
+    def load_init_terms(self) -> Tuple[np.ndarray, np.ndarray]:
+        s = np.zeros((self.gConf['nx'], self.gConf['ny'],
+                      self.gConf['nz'], self.gConf['ng']))
+        T = np.zeros((self.gConf['nx'], self.gConf['ny'],
+                      self.gConf['nz'], self.gConf['ng']))
+
+        for h in self.hosts:
+            s[h['x'], h['y'], h['z'], [1, 5]] = h['lam']
+            T[h['x'], h['y'], h['z'], :] = (
+                h['T1_0_U'], h['T2_0_U'], h['I_0_U'], h['V_0_U'],
+                h['T1_0_L'], h['T2_0_L'], h['I_0_L'], h['V_0_L'],
+                self.gConf['env_0']
+            )
+
+        return s, T
+
+    def update_A(self, A_diag: np.ndarray, A_off_diag: np.ndarray,
+                 T: np.ndarray, t: float) -> None:
+        def delta(delta_I, sigma, mu, t):
+            return delta_I * np.e ** (sigma * (t - mu)) if (t >= mu) else delta_I
+
+        for h in self.hosts:
+            A_diag[h['x'], h['y'], h['z'], :] = (
+                # T1_U, T2_U
+                h['beta_U'] * T[h['x'], h['y'], h['z'], 3],
+                h['beta_U'] * T[h['x'], h['y'], h['z'], 3],
+                # I_U
+                delta(h['delta_I'], h['sigma_U'], h['mu'], t)
+                + h['w_U'] * T[h['x'], h['y'], h['z'], 1],
+                # V_U
+                h['c_U'],
+                # T1_L, T2_L
+                h['beta_L'] * T[h['x'], h['y'], h['z'], 7],
+                h['beta_L'] * T[h['x'], h['y'], h['z'], 7],
+                # I_L
+                delta(h['delta_I'], h['sigma_L'], h['mu'], t)
+                + h['w_L'] * T[h['x'], h['y'], h['z'], 5],
+                # V_L
+                h['c_L'],
+                # V_env
+                self.gConf['halflife'] + self.gConf['ventilation']
+            )
+
+            A_off_diag[h['x'], h['y'], h['z'],
+                       [2, 2, 3, 3, 6, 6, 7, 7, 8, 8],
+                       [0, 1, 2, 8, 4, 5, 3, 6, 3, 7]
+                       ] = (
+                -h['beta_U'] * T[h['x'], h['y'], h['z'], 3],
+                -h['beta_U'] * T[h['x'], h['y'], h['z'], 3],
+                -h['p_U'], -h['a_inhale'],
+                -h['beta_L'] * T[h['x'], h['y'], h['z'], 7],
+                -h['beta_L'] * T[h['x'], h['y'], h['z'], 7],
+                -h['a_conduct'], -h['p_L'],
+                -self.gConf['gamma_U'], -self.gConf['gamma_L']
+            )
+
+    def inhost_viral_model_solver(self):
+        """ An inhost viral model solver using `sim_time_steping_diffusion_calc`
+        to solve one or many within-host viral dynamic models simultaneously
+        """
+        # initialise matrices
+        #
+        v_n = np.ones(self.gConf['ng'])
+        # advection-diffusion in the system
+        kdiff = np.zeros((self.gConf['nx'], self.gConf['ny'],
+                          self.gConf['nz'], self.gConf['ng']))
+        kdiff[:, :, :, -1] = 1.e8
+        # advection velocity
+        u = np.zeros((
+            self.conf['ndim_vel'], self.gConf['nx'] + 1, self.gConf['ny'] + 1,
+            self.gConf['nz'] + 1, self.gConf['ng']
+        ))
+        #
+        A_diag = np.zeros((self.gConf['nx'], self.gConf['ny'],
+                           self.gConf['nz'], self.gConf['ng']))
+        A_off_diag = np.zeros((
+            self.gConf['nx'], self.gConf['ny'], self.gConf['nz'],
+            self.gConf['ng'], self.gConf['ng2']
+        ))
+        # source term and initial values
+        s, T = self.load_init_terms()
+
+        time_list = [0]
+        T_list = [T]
+        for i in range(self.conf['nstep']):
+            print(f"{i + 1}/{self.conf['nstep']}", end='          \r', flush=True)
+            t = time_list[-1] + self.conf['dt'] * self.conf['ntime']
+            self.update_A(A_diag, A_off_diag, T, t)
+
+            T = dv.sim_time_steping_diffusion_calc(
+                T, self.conf['ntime'], self.conf['nits'], self.conf['nits_solv_ng'],
+                self.conf['relax'], self.gConf['error_solv'],
+                self.gConf['error_solv_ng'],
+                self.gConf['dx'], self.gConf['dy'], self.gConf['dz'], self.conf['dt'],
+                v_n, A_diag, A_off_diag, kdiff, s, u,
+                self.gConf['i_upwind'], self.gConf['i_harmonic'],
+                self.conf['ndim_vel'], self.gConf['nx'], self.gConf['ny'], self.gConf['nz'],
+                self.gConf['ng'], self.gConf['ng2']
+            )
+
+            time_list.append(t)
+            T_list.append(T)
+        print()
+
+        return np.array(T_list), time_list
+
+    @classmethod
+    def loadConst(cls) -> None:
+        try:
+            with open(cls.constPath, 'r') as f:
+                const = json.load(f)
+            cls.T1_0 = const['T1_0']
+            cls.T2_0 = const['T2_0']
+            cls.I_0 = const['I_0']
+            cls.lam = const['lam']
+            cls.delta_I = const['delta_I']
+        except KeyError as err:
+            logging.error(err)
+            sys.exit(1)
+        except FileNotFoundError as err:
+            logging.error(err)
+            sys.exit(1)
+
+    @classmethod
+    def loadModel(cls, conf: pathlib.Path) -> None:
+        try:
+            with open(conf, 'r') as f:
+                conf = json.load(f)
+            cls.d_sec = conf['d_sec']  # amount of second for each time step
+            cls.t_sec = conf['t_day'] * 86400  # amount of second
+            cls.ntime = conf['ntime']  # output every n step
+            cls.conf = {
+                'dt': cls.d_sec / 86400,  # convert time step unit to day
+                'ntime': conf['ntime'],
+                'nstep': math.ceil(cls.t_sec / cls.d_sec / cls.ntime),  # steps
+                'ndim_vel': conf['ndim_vel'],
+                'nits': conf['nits'],
+                'nits_solv_ng': conf['nits_solv_ng'],
+                'relax': conf['relax']
+            }
+        except KeyError as err:
+            logging.error(err)
+            sys.exit(1)
+        except FileNotFoundError as err:
+            logging.error(err)
+            sys.exit(1)
+
+    @classmethod
+    def loadHosts(cls, conf: pathlib.Path) -> None:
+        try:
+            with open(conf, 'r') as f:
+                conf = json.load(f)
+            cls.hostAmount = conf['hints']['amount']
+            cls.generateHost(conf['hints']['sick'], conf['hosts'])
+        except KeyError as err:
+            logging.error(err)
+            sys.exit(1)
+        except ValueError as err:
+            logging.error(err)
+            sys.exit(1)
+        except FileNotFoundError as err:
+            logging.error(err)
+            sys.exit(1)
+
+    @classmethod
+    def loadGrid(cls, conf: pathlib.Path) -> None:
+        try:
+            with open(conf, 'r') as f:
+                conf = json.load(f)
+            cls.gConf = {
+                'nx': cls.size * 2 + 1, 'ny': cls.size * 2 + 1, 'nz': 3,
+                'ng': 9, 'ng2': 9,
+                "error_solv": conf['error_solv'],
+                "error_solv_ng": conf['error_solv_ng'],
+                "i_upwind": 1, "i_harmonic": 0,
+                "dx": 0.1, "dy": 0.1, "dz": 0.1,
+                "env_0": conf['env_0'], "halflife": conf['halflife'],
+                "ventilation": conf['ventilation'],
+                "gamma_U": conf['gamma_U'], "gamma_L": conf['gamma_L']
+            }
+        except KeyError as err:
+            logging.error(err)
+            sys.exit(1)
+        except FileNotFoundError as err:
+            logging.error(err)
+            sys.exit(1)
+
+    @classmethod
+    def generateHost(cls, sick: int, hosts: List[Dict]) -> None:
+        for h in hosts:
+            h['T1_0_U'] = cls.T1_0
+            h['T1_0_L'] = cls.T1_0
+            h['T2_0_U'] = cls.T2_0
+            h['T2_0_L'] = cls.T2_0
+            h['I_0_U'] = cls.I_0
+            h['I_0_L'] = cls.I_0
+            h['lam'] = cls.lam
+            h['delta_I']= cls.delta_I
+            if h['V_0_U'] > 0 or h['V_0_L'] > 0:
+                sick -= 1
+        if sick < 0:
+            raise ValueError('number of sick hosts exceed hints.sick')
+
+        for _ in range(sick):
+            hosts.append(cls.newHost(sick=True))
+        while len(hosts) < cls.hostAmount:
+            hosts.append(cls.newHost())
+        cls.placeHost(hosts)
+
+    @classmethod
+    def newHost(cls, sick: bool = False) -> Dict:
+        h = {
+            'T1_0_U': cls.T1_0, 'T1_0_L': cls.T1_0,
+            'T2_0_U': cls.T2_0, 'T2_0_L': cls.T2_0,
+            'I_0_U': cls.I_0, 'I_0_L': cls.I_0,
+            'lam': cls.lam, 'delta_I': cls.delta_I,
+            'V_0_U': 0, 'V_0_L': 0,
+            'beta_U': 0, 'p_U': 0, 'c_U': 0,
+            'w_U': 0, 'sigma_U': 0,
+            'beta_L': 0, 'p_L': 0, 'c_L': 0,
+            'w_L': 0, 'sigma_L': 0,
+            'mu': 0,
+            'a_conduct': 0, 'a_inhale': 0
+        }
+
+        if sick:
+            h['V_0_U']: 1.e-4
+            h['V_0_L']: 1.e-3
+        return h
+
+    @classmethod
+    def placeHost(cls, hosts: List[Dict]) -> None:
+        cls.size = math.ceil(math.sqrt(cls.hostAmount))
+        xs = [i // cls.size * 2 + 1 for i in range(len(hosts))]
+        ys = [i % cls.size * 2 + 1 for i in range(len(hosts))]
+        for h, x, y in zip(hosts, xs, ys):
+            h['x'] = x
+            h['y'] = y
+            h['z'] = 1
+        cls.hosts = hosts
+
+
 if __name__ == '__main__':
     args, parser = parseInput()
 
-    with open(args.modelConf, 'r') as f:
-        args.modelConf = json.load(f)
+    model = Model(args.modelConf, args.hostsConf, args.gridConf)
 
-    # global constants (Wang, S. et al. 2020)
-    T1_0 = args.modelConf['T1_0']
-    T2_0 = args.modelConf['T2_0']
-    I_0 = args.modelConf['I_0']
-    lam = args.modelConf['lam']
-    delta_I = args.modelConf['delta_I']
-
-    # hyper-param
-    d_sec = args.modelConf['d_sec']  # amount of second for each time step
-    t_sec = args.modelConf['t_day'] * const_sec_per_day  # amount of second
-    ntime = args.modelConf['ntime']  # output every n step
-
-    # prepare model configuration
-    del args.modelConf['T1_0']
-    del args.modelConf['T2_0']
-    del args.modelConf['I_0']
-    del args.modelConf['lam']
-    del args.modelConf['delta_I']
-    del args.modelConf['d_sec']
-    del args.modelConf['t_day']
-    args.modelConf['dt'] = d_sec / const_sec_per_day  # convert time step unit
-    args.modelConf['nstep'] = math.ceil(t_sec / d_sec / ntime)  # total steps
-
-    # prepare grid configuration
-    with open(args.gridConf, 'r') as f:
-        args.gridConf = json.load(f)
-
-    # prepare hosts configuration
-    with open(args.hostConf, 'r') as f:
-        args.hostConf = json.load(f)
-    for i in range(len(args.hostConf)):
-        args.hostConf[i]['T1_0_U'] = T1_0
-        args.hostConf[i]['T1_0_L'] = T1_0
-        args.hostConf[i]['T2_0_U'] = T2_0
-        args.hostConf[i]['T2_0_L'] = T2_0
-        args.hostConf[i]['I_0_U'] = I_0
-        args.hostConf[i]['I_0_L'] = I_0
-        args.hostConf[i]['lam'] = lam
-        args.hostConf[i]['delta_i'] = delta_I
-
-    """
-    a_inhale_list = [2 ** -i for i in range(3)] + [0]
     URTs = []
     LRTs = []
-    for a_inhale in a_inhale_list:
-        hosts[0]['a_inhale'] = a_inhale
-        T_list, t_list = inhost_viral_model_solver(
-            args.modelConf, args.gridConf, hosts
-        )
-        URTs.append(T_list[:, 1, 1, 1, 3])
-        LRTs.append(T_list[:, 1, 1, 1, 7])
+    T_list, t_list = model.inhost_viral_model_solver()
+    URTs.append(T_list[:, 1, 1, 1, 3])
+    LRTs.append(T_list[:, 1, 1, 1, 7])
 
     saveDict = {
         'URTs': URTs,
         'LRTs': LRTs,
-        't_list': t_list,
-        'a_inhale_list': a_inhale_list
+        't_list': t_list
     }
     with open('tmp.pkl', 'wb') as f:
         pickle.dump(saveDict, f, pickle.HIGHEST_PROTOCOL)
-    """
--- plotting.py
+++ plotting.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-# python version 3.6.14
+# encoding: utf-8
+# version: 3.8.2
 
 # standard modules
 import pickle
@@ -11,21 +12,18 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     with open('tmp.pkl', 'rb') as f:
         loadDict = pickle.load(f)
-    URTs = loadDict['URTs']
-    LRTs = loadDict['LRTs']
+    urt = loadDict['URTs'][0]
+    lrt = loadDict['LRTs'][0]
     t_list = loadDict['t_list']
-    a_inhale_list = loadDict['a_inhale_list']
 
-    fig, axs = plt.subplots(2, 2, figsize=(24, 18))
-    for ax, urt, lrt, a_conduct in zip(axs.flatten(), URTs, LRTs, a_inhale_list):
-        ax.semilogy(t_list, urt, label='URT')
-        ax.semilogy(t_list, lrt, label='LRT')
-        ax.plot([-5, 35], [1e2, 1e2], ':')
-        ax.set_xlim(-1, 31)
-        ax.set_ylim(1, 1e14)
-        ax.set_xlabel('time (day)')
-        ax.set_ylabel('viral load')
-        ax.set_title(f'conduct={a_conduct}')
-        ax.legend(loc='best')
+    fig, ax = plt.subplots(1, 1, figsize=(24, 18))
+    ax.semilogy(t_list, urt, label='URT')
+    ax.semilogy(t_list, lrt, label='LRT')
+    ax.plot([-5, 35], [1e2, 1e2], ':')
+    ax.set_xlim(-1, 31)
+    ax.set_ylim(1, 1e14)
+    ax.set_xlabel('time (day)')
+    ax.set_ylabel('viral load')
+    ax.legend(loc='best')
     plt.tight_layout()
     plt.savefig('tmp.png')
